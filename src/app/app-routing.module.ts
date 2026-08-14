489d44f45e006006f59f87880e9b09eef4a6f9cc
snehith <msnehithbabu2007@gmail.com>
2026-08-14 12:08:41 +0530

    Netflix clone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent},
+  { path: 'welcome', component: WelcomeComponent},
+  { path: '**', component: PagenotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
